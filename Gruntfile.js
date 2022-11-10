3f6dfc4d46e5088e33098cc2ab1690e5e044cbca
David Burke <dburke@k-state.edu>
2022-11-10 13:59:13 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,57 +5,24 @@ module.exports = function(grunt) {
     //Configuration
   grunt.initConfig({
       //pass in options to plugins, references to files etc
-    pkg: grunt.file.readJSON('package.json'),
+   // pkg: grunt.file.readJSON('package.json'),
     concat: {
-      options: {
-        separator: ';'
+      js: {
+        src: ['js/*.js'],
+          dest:'build/scripts.js'
       },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+        css: {
+        src: ['css/*.css'],
+          dest:'build/styles.css'
       }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
     }
   });
 
     // Load plugins
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Register tasks
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('concat-js', ['concat:js']);
+  grunt.registerTask('concat-css', ['concat:css']);
 
 };
