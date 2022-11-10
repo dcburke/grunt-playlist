6c04b88dddfb5d4a99cc9f159af640951108cdb0
David Burke <dburke@k-state.edu>
2022-11-10 15:12:33 -0600

    Added Uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,36 @@
 // JavaScript Document
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    //Configuration
+  //Configuration
   grunt.initConfig({
-      //pass in options to plugins, references to files etc
-   //pkg: grunt.file.readJSON('package.json'),
+    //pass in options to plugins, references to files etc
+    //pkg: grunt.file.readJSON('package.json'),
     concat: {
       js: {
         src: ['js/*.js'],
-          dest:'build/scripts.js'
+        dest: 'build/scripts.js'
       },
-        css: {
+      css: {
         src: ['css/*.css'],
-          dest:'build/styles.css'
+        dest: 'build/styles.css'
+      }
+    },
+    uglify: {
+      build: {
+        files: [{
+          src: 'build/scripts.js',
+          dest: 'build/scripts.js'
+        }]
       }
     }
   });
 
-    // Load plugins
+  // Load plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Register tasks
+  // Register tasks
   grunt.registerTask('concat-js', ['concat:js']);
   grunt.registerTask('concat-css', ['concat:css']);
 
